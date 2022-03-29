22bb76a5e09430f0bed832f3330680f0dcf17cdd
Jaechang Namgoong <jnamgoong@iconloop.com>
2022-03-29 15:19:24 +0900

    Add iusdc and usds

--- score/token.py
+++ score/token.py
@@ -24,6 +24,8 @@ class IRC2Token(Score):
         'sicx': 'cx2609b924e33ef00b648a409245c7ea394c467824',
         'baln': 'cxf61cd5a45dc9f91c15aa65831a30a90d59a09619',
         'bnusd': 'cx88fd7df7ddff82f7cc735c871dc519838cb235bb',
+        'iusdc': 'cxae3034235540b924dfcc1b45836c293dcc82bfb7',
+        'usds': 'cxbb2871f468a3008f80b08fdde5b8b951583acf06',
         'omm': 'cx1a29259a59f463a67bb2ef84398b30ca56b5830a',
         'cft': 'cx2e6d0fc0eca04965d06038c8406093337f085fcf',
         'gbet': 'cx6139a27c15f1653471ffba0b4b88dc15de7e3267'

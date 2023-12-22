b62e75cae62ba2e63f01c2bfd6eb0fc8d894ca34
Jaechang Namgoong <jnamgoong@parametacorp.com>
2023-12-22 17:32:45 +0900

    Update info command

--- iiss/info.py
+++ iiss/info.py
@@ -12,8 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
+
 from score.chain import ChainScore
-from util import print_response, get_icon_service
+from util import print_response, get_icon_service, die
 from util.txhandler import TxHandler
 
 
@@ -29,18 +31,22 @@ class Info(object):
     def get_network_info(self):
         return self._chain.call("getNetworkInfo")
 
-    def print_next_term(self):
+    def print_next_term(self, end_block=None):
         result = self.get_iiss_info()
         current_block = int(result['blockHeight'], 16)
-        next_term = int(result['nextPRepTerm'], 16)
-        remaining_block = next_term - current_block
+        if not end_block:
+            end_block = int(result['nextPRepTerm'], 16)
+        elif end_block <= current_block:
+            die(f'Error: end_block must be greater than current_block={current_block}')
+        remaining_block = end_block - current_block
         remaining_seconds = remaining_block * 2
         hours = remaining_seconds // 3600
         minutes = (remaining_seconds % 3600) // 60
         seconds = (remaining_seconds % 3600) % 60
         time_left = {
             'Blocks': remaining_block,
-            'Countdown': f'{hours}:{minutes:02d}:{seconds:02d}'
+            'Countdown': f'{hours}:{minutes:02d}:{seconds:02d}',
+            'EndAt': time.ctime(time.time() + remaining_seconds)
         }
         print_response('Remaining Time', time_left)
 
@@ -52,6 +58,7 @@ class Info(object):
 def add_parser(cmd, subparsers):
     info_parser = subparsers.add_parser('info', help='Query IISS Information')
     info_parser.add_argument('--next-term', action='store_true', help='show the remaining time to next term')
+    info_parser.add_argument('--end-block', type=int, help='show the remaining time to end block')
 
     # register method
     setattr(cmd, 'info', run)
@@ -62,5 +69,7 @@ def run(args):
     info = Info(tx_handler)
     if args.next_term:
         info.print_next_term()
+    elif args.end_block:
+        info.print_next_term(args.end_block)
     else:
         info.print_info()
